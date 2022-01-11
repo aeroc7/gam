ba0dd71ae609f06bc6b3fb1dcce768cd93b3ce32
bennett <aerozotac@outlook.com>
2022-01-10 16:56:31 -0800

    File for constants

---
+++ src/utils/constants.h
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2022 Bennett Anderson
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EARTH_RADIUS  6371
+
+#define DEG_TO_RAD(x) ((x) * (M_PI / 180.0))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONSTANTS_H_ */
