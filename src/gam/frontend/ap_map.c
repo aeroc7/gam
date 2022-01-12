068062123893ae72e10eee35d13711187205d7e7
bennett <aerozotac@outlook.com>
2022-01-11 23:07:30 -0800

    Abstract projection method into function, accurate pixel per meter measure

--- src/gam/frontend/ap_map.c
+++ src/gam/frontend/ap_map.c
@@ -43,7 +43,7 @@ haversine_formula_meters(double lat1, double lon1, double lat2, double lon2) {
 
     double a = pow(sin(dlat / 2.0), 2) + pow(sin(dlon / 2.0), 2) * cos(lat1) * cos(lat2);
     double c = 2.0 * asin(sqrt(a));
-    return EARTH_RADIUS * c;
+    return EARTH_RADIUS * c * 1000.0;
 }
 
 static void
@@ -102,15 +102,60 @@ ap_map_set_draw_dims(ap_map_t *ap, size_t ap_index) {
 }
 
 static double
-ap_map_distance_to_x(ap_map_t *ap, double distance) {
+ap_map_distance_to_x(const ap_map_t *ap, double distance) {
     ASSERT(ap != NULL);
     return ap->draw_w_ratio * distance;
 }
 
 static double
-ap_map_distance_to_y(ap_map_t *ap, double distance) {
+ap_map_distance_to_y(const ap_map_t *ap, double distance) {
     ASSERT(ap != NULL);
     return ap->draw_h_ratio * distance;
+}
+
+static vec2d_t
+ap_map_latlon_project(const ap_map_t *ap, lat2d_t p) {
+    vec2d_t local_cords;
+    double  avg_lat = (ap->map_bounds.lat1 + p.lat) / 2.0;
+    double  avg_lon = (ap->map_bounds.lon1 + p.lon) / 2.0;
+
+    double  d_to_min_lat = haversine_formula_meters(p.lat, avg_lon, ap->map_bounds.lat1, avg_lon);
+    double  d_to_min_lon = haversine_formula_meters(avg_lat, p.lon, avg_lat, ap->map_bounds.lon1);
+
+    /* Also flips coordinates over y-axis */
+    local_cords.x = ap_map_distance_to_x(ap, d_to_min_lat);
+    local_cords.y = ap_map_distance_to_y(ap, d_to_min_lon);
+
+    return local_cords;
+}
+
+static double
+ap_map_pixels_per_meter(const ap_map_t *ap, size_t ap_index) {
+    ASSERT(ap != NULL);
+    const airport_info_t *ap_db = &ap->db->airports[ap_index];
+    ASSERT(ap_db->runways_size > 0);
+
+    const lat2d_t p1 = {
+        .lat = ap_db->runways[0].latitude[0], .lon = ap_db->runways[0].longitude[0]};
+    const lat2d_t p2 = {
+        .lat = ap_db->runways[0].latitude[1], .lon = ap_db->runways[0].longitude[1]};
+
+    double  real_dist_meters = haversine_formula_meters(p1.lat, p1.lon, p2.lat, p2.lon);
+
+    vec2d_t lp1 = ap_map_latlon_project(ap, p1);
+    vec2d_t lp2 = ap_map_latlon_project(ap, p2);
+
+    /* Distance formula */
+    double rwy_loc_delta_x2 = pow(lp2.x - lp1.x, 2);
+    double rwy_loc_delta_y2 = pow(lp2.y - lp1.y, 2);
+    double xy_distance = sqrt(rwy_loc_delta_x2 + rwy_loc_delta_y2);
+
+    return (xy_distance / real_dist_meters);
+}
+
+static double
+ap_map_meters_to_pixels(const ap_map_t *ap, size_t ap_index, double meters) {
+    return ap_map_pixels_per_meter(ap, ap_index) * meters;
 }
 
 void
@@ -119,24 +164,17 @@ ap_map_draw(cairo_t *cr, ap_map_t *ap, size_t ap_index) {
 
     for (size_t i = 0; i < ap->db->airports[ap_index].runways_size; ++i) {
         const runway_info_t *rwy = &ap->db->airports[ap_index].runways[i];
-        double               d_to_min_lat[2];
-        double               d_to_min_lon[2];
+        vec2d_t              rwy_coords[2];
 
         for (int j = 0; j < 2; ++j) {
-            double avg_lat = (ap->map_bounds.lat1 + rwy->latitude[j]) / 2.0;
-            double avg_lon = (ap->map_bounds.lon1 + rwy->longitude[j]) / 2.0;
-
-            d_to_min_lat[j] =
-                haversine_formula_meters(rwy->latitude[j], avg_lon, ap->map_bounds.lat1, avg_lon);
-            d_to_min_lon[j] =
-                haversine_formula_meters(avg_lat, rwy->longitude[j], avg_lat, ap->map_bounds.lon1);
+            lat2d_t p = {.lat = rwy->latitude[j], .lon = rwy->longitude[j]};
+            rwy_coords[j] = ap_map_latlon_project(ap, p);
         }
 
         cairo_set_source_rgb(cr, 0, 0.25, 0.75);
-        cairo_move_to(cr, ap_map_distance_to_x(ap, d_to_min_lat[0]),
-            ap_map_distance_to_y(ap, d_to_min_lon[0]));
-        cairo_line_to(cr, ap_map_distance_to_x(ap, d_to_min_lat[1]),
-            ap_map_distance_to_y(ap, d_to_min_lon[1]));
+        cairo_set_line_width(cr, ap_map_meters_to_pixels(ap, ap_index, rwy->width));
+        cairo_move_to(cr, rwy_coords[0].x, rwy_coords[0].y);
+        cairo_line_to(cr, rwy_coords[1].x, rwy_coords[1].y);
         cairo_stroke(cr);
     }
 }
